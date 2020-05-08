ed4edd02e7d64d210057b9e1d1d530c6ebd54440
Leandro Martins <leandro.martins.85@gmail.com>
2020-05-08 00:18:11 -0300

    Validando credenciais | Gerando token JWT

---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,45 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+
+import User from '../models/User';
+
+interface Request {
+  email: string;
+  password: string;
+}
+
+interface Response {
+  user: User;
+  token: string;
+}
+
+class AuthenticateUsersService {
+  public async execute({ email, password }: Request): Promise<Response> {
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new Error('Invalid email or password.');
+    }
+
+    const passwordMatched = await compare(password, user.password);
+
+    if (!passwordMatched) {
+      throw new Error('Invalid email or password.');
+    }
+
+    const token = sign({}, 'b4b26dbf602a54a5f81667d08066a5fd', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return {
+      user,
+      token,
+    };
+  }
+}
+
+export default AuthenticateUsersService;
