e9dbde0569878dd26bb9783bbab868dbed59a8ed
Leandro Martins <leandro.martins.85@gmail.com>
2020-05-10 18:37:35 -0300

    Criando classe de erro

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,9 +1,10 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
-
 import authConfig from '../config/auth';
 import User from '../models/User';
+
+import AppError from '../error/AppError';
 
 interface Request {
   email: string;
@@ -22,13 +23,13 @@ class AuthenticateUsersService {
     const user = await usersRepository.findOne({ where: { email } });
 
     if (!user) {
-      throw new Error('Invalid email or password.');
+      throw new AppError('Invalid email or password.', 401);
     }
 
     const passwordMatched = await compare(password, user.password);
 
     if (!passwordMatched) {
-      throw new Error('Invalid email or password.');
+      throw new AppError('Invalid email or password.', 401);
     }
 
     const { secret, expiresIn } = authConfig.jwt;
--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -1,6 +1,8 @@
 import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 
+import AppError from '../error/AppError';
+
 import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
@@ -20,7 +22,7 @@ class CreateAppointmentService {
     );
 
     if (findAppointmentInSameDate) {
-      throw Error('This appointmente is already booked');
+      throw new AppError('This appointmente is already booked');
     }
 
     const appointment = appointmentsRepository.create({
